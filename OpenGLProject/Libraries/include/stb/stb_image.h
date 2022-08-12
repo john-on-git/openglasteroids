e4eb037bcfc61803eadf614e9710abb63c0c3ad1
John Gregg-Webster <john.gregg-webster.2021@uni.strath.ac.uk>
2022-08-12 23:46:33 +0100

    deleted files from git oopsie

--- OpenGLProject/Libraries/include/stb/stb_image.h
+++ OpenGLProject/Libraries/include/stb/stb_image.h
@@ -1314,33 +1314,33 @@ STBIDEF int stbi_convert_wchar_to_utf8(char *buffer, size_t bufferlen, const wch
 }
 #endif
 
-static FILE *stbi__fopen(char const *filename, char const *mode)
+static FILE* stbi__fopen(char const* filename, char const* mode)
 {
-   FILE *f;
+    FILE* f;
 #if defined(_WIN32) && defined(STBI_WINDOWS_UTF8)
-   wchar_t wMode[64];
-   wchar_t wFilename[1024];
-	if (0 == MultiByteToWideChar(65001 /* UTF8 */, 0, filename, -1, wFilename, sizeof(wFilename)/sizeof(*wFilename)))
-      return 0;
+    wchar_t wMode[64];
+    wchar_t wFilename[1024];
+    if (0 == MultiByteToWideChar(65001 /* UTF8 */, 0, filename, -1, wFilename, sizeof(wFilename) / sizeof(*wFilename)))
+        return 0;
 
-	if (0 == MultiByteToWideChar(65001 /* UTF8 */, 0, mode, -1, wMode, sizeof(wMode)/sizeof(*wMode)))
-      return 0;
+    if (0 == MultiByteToWideChar(65001 /* UTF8 */, 0, mode, -1, wMode, sizeof(wMode) / sizeof(*wMode)))
+        return 0;
 
 #if defined(_MSC_VER) && _MSC_VER >= 1400
-	if (0 != _wfopen_s(&f, wFilename, wMode))
-		f = 0;
+    if (0 != _wfopen_s(&f, wFilename, wMode))
+        f = 0;
 #else
-   f = _wfopen(wFilename, wMode);
+    f = _wfopen(wFilename, wMode);
 #endif
 
 #elif defined(_MSC_VER) && _MSC_VER >= 1400
-   if (0 != fopen_s(&f, filename, mode))
-      f=0;
+    if (0 != fopen_s(&f, filename, mode))
+        f = 0;
 #else
-   f = fopen(filename, mode);
+    f = fopen(filename, mode);
 #endif
-   return f;
-}
+    return f;
+};
 
 
 STBIDEF stbi_uc *stbi_load(char const *filename, int *x, int *y, int *comp, int req_comp)
